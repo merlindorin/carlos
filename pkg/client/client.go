232a072ea10599183d622e452eed1961f22aad97
merlin <romain.dary@renault.com>
2019-11-09 06:58:25 +0100

    move internal

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,18 +1,18 @@
 package client
 
 import (
-	"github.com/iam-merlin/carlos/internal/grpc"
+	"github.com/iam-merlin/carlos"
 	grpc2 "google.golang.org/grpc"
 )
 
 type Client struct {
-	Car grpc.CarServiceClient
-	Log grpc.LogServiceClient
+	Car main.CarServiceClient
+	Log main.LogServiceClient
 }
 
 func NewClient(conn *grpc2.ClientConn) Client {
-	c := grpc.NewCarServiceClient(conn)
-	l := grpc.NewLogServiceClient(conn)
+	c := main.NewCarServiceClient(conn)
+	l := main.NewLogServiceClient(conn)
 
 	return Client{
 		Car: c,
